3fe406adc08e02535c7086c23e62aafa3144e347
rkmad <rmadisetti3@gatech.edu>
2018-10-09 21:01:55 -0400

    modifies scroll.js and index.html

--- scroll.js
+++ scroll.js
@@ -1,62 +1,53 @@
-var today = new Date();
+var dayOfWeek;
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']; //array of the days of the week
+
+//function to get the day of the week based on the date Ex: getDayOfWeek('03', '12', '2013')
+const getDayOfWeek = function(month, day, year) {
+    var day = new Date(`${month}/${day}/${year}`);
+    var dayNum = day.getDay();
+    dayOfWeek = days[dayNum];
+    return dayOfWeek
+};
+
+
+//today's date and all the parameters of the date and time
+var today = new Date(); 
 var dd = today.getDate();
 var mm = today.getMonth()+1; 
 var yyyy = today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var todayDayOfWeek = getDayOfWeek(mm, dd, yyyy);
 
+//array of the names of the months and monthName is assigned to current month
 const arrMonth = ["","January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
 var monthName = arrMonth[mm];
 
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-var dayNum = today.getDay();
-var dayOfWeek = days[dayNum];
+// get the day of week of the first day of the month
+var day1MonthDOW = getDayOfWeek(mm ,'01', yyyy);
 
-var dayTotal = 31;
 
-if (monthName == "February") {
-    if (yyyy % 4 == 0) {
-        dayTotal = 29;
-    }
-    else {
-        dayTotal = 28;
-    }
-}
-if (monthName == "April") {
-    dayTotal = 30;
-}
-if (monthName == "June") {
-    dayTotal = 30;
-}
-if (monthName == "September") {
-    dayTotal = 30;
-}
-if (monthName == "November") {
-    dayTotal = 30;
-}
+// var dayTotal = 31;
+// if (monthName == "February") { //Assigns month name to variable monthName
+//     if (yyyy % 4 == 0) {
+//         dayTotal = 29;
+//     }
+//     else {
+//         dayTotal = 28;
+//     }
+// }
+// if (monthName == "April") {
+//     dayTotal = 30;
+// }
+// if (monthName == "June") { 
+//     dayTotal = 30;
+// }
+// if (monthName == "September") {
+//     dayTotal = 30;
+// }
+// if (monthName == "November") {
+//     dayTotal = 30;
+// }
 
-var arrDaysOfMonth = [];
-for (let i = 1; i <= dayTotal; i++) {
-    arrDaysOfMonth[i] = i;
-}
-arrDaysOfMonth[dd] = dayofWeek;
-var dayWeekCount = dayNum;
-for (let i = dd + 1; i <= dayTotal; i++) {
-    if (dayWeekCount != 6) {
-        dayWeekCount += 1;
-    }
-    else {
-        dayWeekCount = 0;
-    }
-    arrDaysOfMonth[i] = days[dayWeekCount];
-}
-
-var dayWeekCount = dayNum;
-for (let i = dd - 1; i >= 0; i--) {
-    if (dayWeekCount != 6) {
-        dayWeekCount -= 1;
-    }
-    else {
-        dayWeekCount = 6;
-    }
-    arrDaysOfMonth[i] = days[dayWeekCount];
-}
+// var arrDaysOfMonth = [];
+// for (let i = 1; i <= dayTotal; i++) {//fill each position in the array with the number of the date
+//     arrDaysOfMonth[i] = i;
+// }
