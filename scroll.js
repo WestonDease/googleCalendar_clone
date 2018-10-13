cf87a310422fc14bdfbc177bcceab633f6e1b227
rkmad <rmadisetti3@gatech.edu>
2018-10-13 13:59:24 -0400

    scroll.js update #2

--- scroll.js
+++ scroll.js
@@ -40,13 +40,13 @@ let todayNumberDayOfWeek = getNumberDayOfWeek(mm, dd, yyyy);
 //array of the names of the months and monthName is assigned to current month
 const arrMonth = ["","January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
 let monthName = arrMonth[mm];
-const arrDayTotal = [0, 31 , 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const arrDayTotal = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 //fills in empty html objects
 $('.month').html(`${monthName}`);
 $('.year').html(`${yyyy}`);
 $(`.date${todayNumberDayOfWeek}`).html(`${dd}`);
- 
+
 //loop to fill in the rest of the week
 let z = 0; //counter variable that holds the value of a date
 for (let i = todayNumberDayOfWeek+1; i <= 7; i++) {
@@ -59,6 +59,7 @@ for (let i = todayNumberDayOfWeek+1; i <= 7; i++) {
         $(`.date${diff+todayNumberDayOfWeek}`).html(`${z}`);
     }
 }
+
 z = arrDayTotal[mm-1] + 1;
 for (let i = todayNumberDayOfWeek-1; i >= 1; i--) {
     let diff = todayNumberDayOfWeek - i;
@@ -75,7 +76,60 @@ for (let i = todayNumberDayOfWeek-1; i >= 1; i--) {
  //function to run when right arrow is clicked
 $('#arrowright').on('click', function (event) {
     event.preventDefault();
-    
+    dd = dd + 7;
+    $(`.date${todayNumberDayOfWeek}`).html(`${dd}`);
+    z = 0;
+    for (let i = todayNumberDayOfWeek+1; i <= 7; i++) {
+        let diff = i - todayNumberDayOfWeek;
+        if ((dd+diff) < arrDayTotal[mm]) {
+            $(`.date${diff+todayNumberDayOfWeek}`).html(`${dd+diff}`);
+        }
+        else {
+            z = z + 1;
+            $(`.date${diff+todayNumberDayOfWeek}`).html(`${z}`);
+        }
+    }
+    z = 0;
+    for (let i = 1; i <= todayNumberDayOfWeek; i++) {
+        let diff = todayNumberDayOfWeek - i;
+        if (dd-diff <= arrDayTotal[mm]) {
+            $(`.date${todayNumberDayOfWeek-diff}`).html(`${dd-diff}`);
+        }
+        else {
+            z = z + 1;
+            $(`.date${todayNumberDayOfWeek-diff}`).html(`${z}`);
+            mm = mm + 1;
+        }
+    }
+
+});
+
+//function to run when left arrow is clicked
+$('#arrowleft').on('click', function (event) {
+    event.preventDefault();
+    dd = dd - 7;
+    $(`.date${todayNumberDayOfWeek}`).html(`${dd}`);
+    for (let i = todayNumberDayOfWeek+1; i <= 7; i++) {
+        let diff = i - todayNumberDayOfWeek;
+        if (dd+diff < arrDayTotal[mm]) {
+            $(`.date${diff+todayNumberDayOfWeek}`).html(`${dd+diff}`);
+        }
+        else {
+            z = z + 1;
+            $(`.date${diff+todayNumberDayOfWeek}`).html(`${z}`);
+        }
+    }
+    z = arrDayTotal[mm-1] + 1;
+    for (let i = 1; i <= todayNumberDayOfWeek; i++) {
+        let diff = todayNumberDayOfWeek - i;
+        if (dd-diff > 0) {
+            $(`.date${todayNumberDayOfWeek-diff}`).html(`${dd-diff}`);
+        }
+        else {
+            z = z - 1;
+            $(`.date${todayNumberDayOfWeek-diff}`).html(`${z}`);
+        }
+    }
 });
 
 // get the day of week of the first day of the month
