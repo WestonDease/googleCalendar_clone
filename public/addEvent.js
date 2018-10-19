f8a212dcafeebdb59e2a519852849dd2fd9f82d3
stonercough <stoner@macbookpro.local>
2018-10-18 21:34:19 -0400

    Adds click.js and post routes for addevent.js

--- public/addEvent.js
+++ public/addEvent.js
@@ -5,6 +5,20 @@ function openForm() {
 function closeForm() {
   document.getElementById("AddEventForm").style.display = "none";
 }
-$(function () {
-  
-});
+const saveEvent = function (e) {
+  e.preventDefault();
+  const eventName = $('#event-name').val();
+  const eventDesc = $('#event-desc').val();
+  const eventDate = $('#event-date').val();
+  const eventTime = $('#event-time').val();
+
+  const data = {
+    title: eventName,
+    description: eventDesc,
+    date: eventDate,
+    time: eventTime
+  }
+  $.post('api/events', data).then(function (data) {});
+}
+
+$('#save-event').on('click', saveEvent);
